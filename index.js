403d1743a09573379cb9607e08e3984504ed2707
Artem Holoven <artem.holoven@gmail.com>
2023-07-14 15:21:30 +0100

    add form functionality

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ const servHeader = document.querySelector('.services-header');
 const servContent = document.querySelector('.services-content');
 const servLeftArrow = document.querySelector('.serv-left-arrow');
 const servRightArrow = document.querySelector('.serv-right-arrow');
+const forms = document.querySelectorAll('.form');
 
 const bottomOfHeader = header.offsetTop + header.offsetHeight;
+
+emailjs.init('ax7ySooPu_KFomJRO');
 
 quoteButtons.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -41,6 +44,36 @@ pageLinks.forEach(link => {
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('opened');
     menu.classList.toggle('opened');
+});
+
+forms.forEach(form => {
+    const counter = form.querySelector('.form-input-symbols-counter');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        if (!form.fullName?.value || !form.email?.value || !form.companyName?.value) return;
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // emailjs.sendForm('service_svvy1sj', 'template_c68wutp', this)
+        //     .then(function() {
+        //         form.reset();
+        //         console.log('SUCCESS!');
+        //     }, function(error) {
+        //         console.log('FAILED...', error);
+        //     });
+        form.classList.add('sent');
+        const p = document.createElement('p');
+        p.innerText = 'Your request has been sent. Our team will contact you as soon as possible.';
+        form.appendChild(p);
+    });
+    form.addEventListener('change', function (event) {
+        event.target.value = event.target.value.trim();
+        counter.innerText = form.comment.value.length;
+    });
+    form.email.addEventListener('blur', function (event) {
+        event.target.value = event.target.value.trim();
+    });
+    form.comment.addEventListener('input', function (event) {
+        counter.innerText = form.comment.value.length;
+    });
 });
 
 window.addEventListener('scroll', () => {
